bf9e9b4858ffa691eb10858abfd31edfbe1ea524
TheInfinity007 <ramchandra71999@gmail.com>
2020-03-17 01:08:54 +0530

    add Print_tree_level_wise.java

---
+++ Binary_Tree/Print_tree_level_wise.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+
+        System.out.println("Enter the no of nodes");
+        n = sc.nextInt();
+
+        System.out.println("Enter the elements");
+        int arr[]=new int[n];
+        for(i=0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        Node root = null;
+        root = buildTree(arr, n);
+
+        System.out.println("\nPrinting Tree with Level Wise Traversal");
+        printLevelWise(root);
+    }
+
+    static void printLevelWise(Node root)
+    {
+        Queue<Node> q = new LinkedList<Node>();
+        Node node = null;
+        q.add(root);
+        q.add(node);
+        Node curr = q.poll();
+        while(q.size() != 0)
+        {
+            if(curr == null)
+            {
+                curr = q.peek();
+            }
+            else
+            {
+                q.add(curr.left);
+                q.add(curr.right);
+                
+                if(q.peek() == null){
+                    System.out.println(curr.data);
+                    q.add(null);
+                }      	
+                else
+                    System.out.print(curr.data + " ");      
+            }      
+            curr = q.poll();
+        }
+    }
+
+
+    static Node buildTree(int arr[], int n)
+    {
+        Node node = null;
+        if(n < 1)               //if array is empty;
+            return node;
+        Queue <Node> q = new LinkedList<Node>();
+        Node root = new Node(arr[0]);       //add first element to the root
+        q.add(root);
+        node = root;
+        
+        for(int i = 1; i < n; i++)
+        {
+            if(node.left == null)
+            {
+                node.left = new Node(arr[i]);
+                q.add(node.left);
+            }
+            else
+            {
+                node.right = new Node(arr[i]);
+                q.add(node.right);
+                q.poll();
+                node = q.peek();
+            }
+        }
+        return root;
+    }
+}
+class Node
+{
+  int data; // data used as key value
+  Node left;
+  Node right;
+  public Node(){};
+  public Node(int d)
+  {
+    data=d;
+  }
+}
